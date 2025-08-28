05b27ba72d8677bc3051b5065e224a44b46f8b26
BrodieKlimek <bklimek2@huskers.unl.edu>
2025-08-28 09:48:43 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Brodie Klimek
+ *         bklimek2@huskers.unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brodie Klimek\n");
+
+  return 0;
+}
