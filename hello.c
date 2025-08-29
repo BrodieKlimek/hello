2d11d72af8c7553a670b9c5c50fb4b14e0b30029
CharlesSanders07 <bklimekord@gmail.com>
2025-08-29 18:06:47 +0000

    Fri, Aug 29, 2025, 6:06 PM +00:00

--- hello.c
+++ hello.c
@@ -14,5 +14,8 @@ int main(int argc, char **argv) {
   printf("Hello world, I'm Brodie Klimek.\n");
   printf("My major is Computer Engineering.\n");
 
+  printf("Hello world, I'm Charles Sanders!\n");
+  printf("My major is Electrical Engineering!\n");
+
   return 0;
 }
